7221be7a66975e21da2e401f1120f58791b2c80a
Marcus Vitor da Cunha Nascimento <106033361+marcusscunha@users.noreply.github.com>
2022-06-14 19:57:47 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+package aplicacao;
+
+import java.util.Locale;
+
+public class Main{
+	public static void main(String[]args) {
+		String product1="Computer";
+		String procudt2="Office Desk";
+		int age=30;
+		int code=5290;
+		char gender='F';
+		
+		double price1=2100.0;
+		double price2=650.50;
+		double measure=53.234567;
+		
+		System.out.println("Products:");
+		System.out.printf("%n%s,which price is $%.2f",product1,price1);
+		System.out.printf("%nRecord:%d years old, code %d and gender:%s", age, code, gender);
+		System.out.printf("%n Measure with eight decimal places:%f",measure);
+		System.out.printf("%nRouderd (three decimal places):%.3f",measure);
+		System.out.printf("%nUS decimal point:%.3f",measure);
+		Locale.setDefault(Locale.US);
+	}
+}
